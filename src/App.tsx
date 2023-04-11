506a9ae7f5c224c6bd0af3b232995f0f09687ad0
ergunDevv <ergncyln2127088@gmail.com>
2023-04-11 11:11:11 +0300

    New note section added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
+import NewNote from './components/NewNote'
 export default function App() {
   return (
     <Container className="my-4">
 
     <Routes>
       <Route path="/" element={<div>hey</div>} />
-      <Route path="/new" element={<h1>new</h1>} />
+      <Route path="/new" element={<NewNote/>} />
 
       <Route path="/:id" >
         <Route index element={<h1>Show</h1>}/>
